73161ead89e28a35dbf39dfa172df3c13d1f2056
kimc <tjdtlr12349@gmail.com>
2024-02-13 09:09:15 +0900

    commentDAO

---
+++ DaangnProject/src/main/java/kr/ezen/daangn/dao/DaangnCommentDAO.java
@@ -0,0 +1,16 @@
+package kr.ezen.daangn.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import kr.ezen.daangn.vo.DaangnCommentVO;
+
+public interface DaangnCommentDAO {
+	List<DaangnCommentVO> selectCommentByUserIdx(int userIdx) throws SQLException;
+	void insertComment(DaangnCommentVO commentVO) throws SQLException;
+	void updateComment(DaangnCommentVO commentVO) throws SQLException;
+	void deleteCommentByIdx(int idx) throws SQLException;
+	void deleteCommentByUserIdx(int userIdx) throws SQLException;
+	
+	float selectEvalByUserIdx(int userIdx) throws SQLException;
+}
---
+++ DaangnProject/src/main/java/kr/ezen/daangn/vo/DaangnCommentVO.java
@@ -0,0 +1,15 @@
+package kr.ezen.daangn.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class DaangnCommentVO {
+	private int idx;			// 키필드
+	private int userIdx;		// 어떤 유저에 대하여
+	private int writerIdx;		// 어떤 유저가
+	private int eval;			// 총점?				// 유저랑 연결!
+	private String content;		// 댓글
+	private Date regDate;		// 댓글쓴 날짜
+}
